fa17cc104deca7bbad9b43e0bfceea3f62b09af0
irrynka <irinakorcinska7@gmail.com>
2026-03-15 19:29:02 +0100

    Added Boostrap,templates,views etc.

--- social_network/users/views.py
+++ social_network/users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+
+def register_view(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
+
+
+def home_view(request):
+    return render(request, 'users/home.html')
