cae9a00d7714d2f3df85b9260edf379fbbfa6a05
irrynka <irinakorcinska7@gmail.com>
2026-03-29 18:03:17 +0200

    Boostrap udpdate and models

--- social_network/users/forms.py
+++ social_network/users/forms.py
@@ -1,16 +1,14 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from .models import User
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
 class LoginForm(AuthenticationForm):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
--- social_network/users/models.py
+++ social_network/users/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    # Щоб не було конфліктів з auth.User
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_groups',  # унікальне ім'я
+        blank=True,
+        help_text='The groups this user belongs to.'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions',  # унікальне ім'я
+        blank=True,
+        help_text='Specific permissions for this user.'
+    )
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    cover_photo = models.ImageField(upload_to='covers/', null=True, blank=True)
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
